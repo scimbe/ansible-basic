f03fc014b2a86f92bf1523a5b9b7c3d4c5d8899c
Martin Becke <scimbe@gmail.com>
2024-03-07 14:11:47 +0100

    Update firewalld-new-docker-interface.sh

--- script/firewalld-new-docker-interface.sh
+++ script/firewalld-new-docker-interface.sh
@@ -11,6 +11,13 @@ fi
 ZONE_NAME="$1"
 INTERFACE="$2"
 
+
+# Überprüfen, ob die Zone korrekt ist
+if  firewall-cmd --get-zones | grep -q "\<$ZONE_NAME\>"; then
+        sudo firewall-cmd --permanent --delete-zone="$ZONE_NAME"
+        sudo firewall-cmd --reload
+fi
+
 sudo firewall-cmd --permanent --new-zone="$ZONE_NAME"
 sudo firewall-cmd --reload
 
@@ -21,19 +28,24 @@ if ! firewall-cmd --get-zones | grep -q "\<$ZONE_NAME\>"; then
 fi
 
 sudo firewall-cmd --zone="$ZONE_NAME" --add-interface="$INTERFACE" --permanent
+sudo  firewall-cmd --reload
 
-sudo firewall-cmd --zone="$ZONE_NAME" --add-masquerade 
-sudo firewall-cmd --permanent --zone="$ZONE_NAME"--set-target=ACCEPT
-sudo firewall-cmd --direct --add-rule ipv4 nat POSTROUTING 0 -o eno1 -j MASQUERADE --permanant
+echo "set zone accept"
+sudo firewall-cmd --permanent --zone="$ZONE_NAME" --add-masquerade  
+sudo firewall-cmd --permanent --zone="$ZONE_NAME" --set-target=ACCEPT 
 
+echo "set masquerade"
+sudo firewall-cmd --permanent  --direct --add-rule ipv4 nat POSTROUTING 0 -o eno1 -j MASQUERADE
+echo "set direct 1"
 sudo firewall-cmd --permanent --direct --add-rule ipv4 filter FORWARD 0 -i eno1 -o "$INTERFACE" -m state --state RELATED,ESTABLISHED -j ACCEPT 
+echo "set direct 2"
 sudo firewall-cmd --permanent --direct --add-rule ipv4 filter FORWARD 0 -i "$INTERFACE" -o eno1  -j ACCEPT 
 sudo  firewall-cmd --reload
-sudo firewall-cmd --list-all --zone="$ZONE_NAME"
 
 echo "Interface $INTERFACE successfully assigned to zone $ZONE_NAME."
 
-sudo firewall-cmd --zone=kasm --add-rich-rule='rule family="ipv4" destination  address="141.22.27.238" drop' --permanent 
+sudo firewall-cmd --zone="$ZONE_NAME"  --add-rich-rule='rule family="ipv4" destination  address="141.22.27.238" drop' --permanent 
+
 
 
 
